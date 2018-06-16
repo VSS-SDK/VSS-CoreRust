ef05fa18b67b6c8d7c3312a6fb823128bd7ea482
johnathan <johnathanfercher22@gmail.com>
2018-06-16 20:57:41 -0300

    working

--- build.rs
+++ build.rs
@@ -5,7 +5,10 @@ fn main() {
     protoc_rust::run(protoc_rust::Args {
         out_dir: "src/protos",
         input: &["protos/command.proto", "protos/control.proto", "protos/debug.proto", "protos/state.proto"],
-        includes: &["protos"]
+        includes: &["protos"],
+        customize: protoc_rust::Customize {
+            ..Default::default()
+        }
     }).expect("protoc");
 
 }
