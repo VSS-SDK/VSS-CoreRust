f4db621651cd96e201132b7ec70f1db0ba625e50
John Fercher <johnathanfercher22@gmail.com>
2018-09-23 14:35:32 -0300

    Adicionado travisCI

---
+++ scripts/install-debian9.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+INSTALL_DEBIAN_9 () {
+    apt-get -y install pkgconf
+    apt-get -y install g++ cmake libzmq5 libzmq3-dev protobuf-compiler libprotobuf-dev libboost-all-dev
+    apt-get -y install cargo
+    INSTALLED=1
+}
